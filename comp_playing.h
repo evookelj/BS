6a42e0d07f22a8ab8c14d64fd7d082f3150bed08
evookelj <evukelj@stuy.edu>
2017-01-07 14:27:37 -0500

    Start human+CPU .h files

---
+++ comp_playing.h
@@ -0,0 +1,12 @@
+#ifndef COMPHEADER_H
+#define COMPHEADER_H
+
+#include "game.h"
+
+int run_cpu_turn(player thisPlayer);
+
+int cpu_cards_and_claim(player thisPlayer);
+
+int cpu_accuse(player thisPlayer);
+
+#endif
---
+++ human_playing.h
@@ -0,0 +1,12 @@
+#ifndef HUMANHEADER_H
+#define HUMANHEADER_H
+
+#include "game.h"
+
+int run_human_turn(player thisPlayer);
+
+int human_cards_and_claim(player thisPlayer);
+
+int human_accuse(player thisPlayer);
+
+#endif
