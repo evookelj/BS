3232cffb3792f78c40ebe824514fbfde387529af
Emma Vukelj <emma.vukelj@gmail.com>
2017-01-10 09:55:54 -0500

    Manually revert

--- profile.h
+++ profile.h
@@ -21,23 +21,23 @@ typedef struct profileStruct profile;
 
 //helper function to take username and sprintf it
 //into correct format to find hidden file holding struct
-char* file_location(char name[30]);
+char* file_location(char* name);
 
 //returns "" upon success, error msg upon failure
 //creates hidden struct file in .profiles for each player profile
 char* create_profile(char* name);
 
 //returns profile of player name
-profile* get_profile(char name[30]);
+profile* get_profile(char* name);
 
 //updates current bs_ratio (lies/total)
-int update_bs_ratio(char name[30], int lies, int total);
+int update_bs_ratio(char* name, int lies, int total);
 
 //isWin=0 if loss, 1 if win for a game
 //updates existing win/loss ratio
-void update_wl_ratio(char name[30], int isWin);
+void update_wl_ratio(char* name, int isWin);
 
 //returns string of profile info formatted
-char* display_profile(char name[30]);
+char* display_profile(char* name);
 
 #endif
