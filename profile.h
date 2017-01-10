1bf6aee27d0c9741d272321a5ee37835bc039d2e
evookelj <evukelj@stuy.edu>
2017-01-10 09:47:38 -0500

    Update header for change of name type

--- profile.h
+++ profile.h
@@ -21,23 +21,23 @@ typedef struct profileStruct profile;
 
 //helper function to take username and sprintf it
 //into correct format to find hidden file holding struct
-char* file_location(char* name);
+char* file_location(char name[30]);
 
 //returns "" upon success, error msg upon failure
 //creates hidden struct file in .profiles for each player profile
 char* create_profile(char* name);
 
 //returns profile of player name
-profile* get_profile(char* name);
+profile* get_profile(char name[30]);
 
 //updates current bs_ratio (lies/total)
-int update_bs_ratio(int lies, int total);
+int update_bs_ratio(char name[30], int lies, int total);
 
 //isWin=0 if loss, 1 if win for a game
 //updates existing win/loss ratio
-void update_wl_ratio(int isWin);
+void update_wl_ratio(char name[30], int isWin);
 
 //returns string of profile info formatted
-char* display_profile(char* name);
+char* display_profile(char name[30]);
 
 #endif
