554aed5124c9a9b861a95d3e862ecc9a325d8f33
evookelj <evukelj@stuy.edu>
2017-01-11 19:43:50 -0500

    Work on (make bugs on) human playin

--- human_playing.c
+++ human_playing.c
@@ -1,16 +1,42 @@
 #include "human_playing.h"
 
-int run_human_turn(player thisPlayer, short currValue) {
+char* getFitting(player* thisPlayer, short currValue) {
+  int i;
+  while (thisPlayer->hand[i]) {
+    card* this = thisPlayer->hand[i];
+    char sval[3];
+    sprintf(sval, "%d of %s", this->value, this->type);
+    printf("%s\n", sval);
+    i++;
+  }
+  return ":O";
+}
+
+int run_human_turn(player* thisPlayer, short currValue) {
   char snum[3];
   sprintf(snum, "%d", currValue);
   if (!strcmp(snum, "1")) {
     strcpy(snum,"A");
   }
-  printf("The current card value for paly is %s. What would you like to do?\n", snum);
+  char input[4];
+  printf("The current card value for play is %s and the cards you have that fit this are %s. What would you like to do: lie (L/l) or BS(B/b)?\n", snum, getFitting(thisPlayer, currValue));
+  fgets(input, sizeof(input), stdin);
+  
   return 0;
 }
 
 int main() {
-  player emma;
+  //ISSUE WITH CARD ADDING THAT I DONT HAVE
+  //TIME TO FIX
+  player* emma;
+  int i;
+  for (i=0; i<5; i++) {
+    strcpy( emma->hand[i]->type, "heart");
+    emma->hand[i]->value = i;
+  }
+  for (i=5; i++; i<17) {
+    emma->hand[i] = NULL;
+  }
   run_human_turn(emma, 1);
+  return 0;
 }
--- human_playing.h
+++ human_playing.h
@@ -3,10 +3,14 @@
 
 #include "game.h"
 
-int run_human_turn(player thisPlayer, short currValue);
+//str listing of cards player has that fit current value
+//so they know what options are upon turn
+char* getFitting(player* thisPlayer, short currValue);
 
-int human_cards_and_claim(player thisPlayer);
+int run_human_turn(player* thisPlayer, short currValue);
 
-int human_accuse(player thisPlayer);
+int human_cards_and_claim(player* thisPlayer);
+
+int human_accuse(player* thisPlayer);
 
 #endif
