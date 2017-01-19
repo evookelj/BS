d8f629e59ea2f809fe76f83b3a260d19d1b0b16e
evookelj <evukelj@stuy.edu>
2017-01-18 21:29:37 -0500

    Fix header

--- player.h
+++ player.h
@@ -7,13 +7,6 @@
 #include "profile.h"
 #include "server.h"
 
-struct fitting_cards_struct {
-  //card** fitting;
-  int count;
-};
-
-typedef struct fitting_cards_struct fitting_cards;
-
 //Starts game for client. Logins to their profile
 char* login();
 
